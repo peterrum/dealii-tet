2aecfe714de841b59c9ad2afa8243406c2d86017
Daniel <28930566+DanielPaukner@users.noreply.github.com>
2020-04-02 12:07:04 +0200

    GridIn read_ucd() is working now. Some fixes and cleanup necessary

--- examples/step-5/tet_test.cc
+++ examples/step-5/tet_test.cc
@@ -46,6 +46,7 @@
 // This one is new. We want to read a triangulation from disk, and the class
 // which does this is declared in the following file:
 #include <deal.II/grid/grid_in.h>
+#include <deal.II/grid/grid_out.h>
 
 // We will use a circular domain, and the object describing the boundary of it
 // comes from this file:
@@ -60,27 +61,25 @@
 using namespace dealii;
 
 template <int dim>
-class Step5
+class Tet_test
 {
 public:
-  Step5();
+	Tet_test();
   void run();
+  void output_results();
 
-private:
-  void output_results() const;
-
-  Triangulation<dim> triangulation;
+  Triangulation<dim,3> triangulation;
 
 };
 
 
 template <int dim>
-Step5<dim>::Step5()
+Tet_test<dim>::Tet_test()
 {}
 
 
 template <int dim>
-void Step5<dim>::output_results() const
+void Tet_test<dim>::output_results()
 {
 //  DataOut<dim> data_out;
 //
@@ -97,65 +96,89 @@ void Step5<dim>::output_results() const
 
 
 template <int dim>
-void Step5<dim>::run()
+void Tet_test<dim>::run()
 {
 	std::cout << "Hello!" << std::endl;
-
-	Tet::Triangulation<dim> tria;
+	const unsigned int spacedim = 3;
+	Triangulation<dim, spacedim> tria;
 
 	  // TODO: create triangulation via GridIn
-	  {
-	    std::vector<Point<dim>>         vertices;
-	    std::vector<Tet::CellData<dim>> cells;
-
-	    vertices.push_back(Point<dim>(1, 0));
-	    vertices.push_back(Point<dim>(0, 1));
-	    vertices.push_back(Point<dim>(0, 0));
-	    vertices.push_back(Point<dim>(1, 1));
-
-	    Tet::CellData<dim> cell_1;
-	    cell_1.type     = Tet::CellTypeEnum::tet;
-	    cell_1.vertices = {0, 1, 2};
-	    cells.push_back(cell_1);
-
-	    Tet::CellData<dim> cell_2;
-	    cell_2.type     = Tet::CellTypeEnum::tet;
-	    cell_2.vertices = {1, 2, 3};
-	    cells.push_back(cell_2);
-
-	    tria.create_triangulation_tet(vertices, cells);
-	  }
-
-	  // print cell-by-cell the vertices
-	  for (const auto &cell : tria.cell_iterators())
-	    {
-	      for (unsigned int i = 0; i < cell->n_vertices(); i++)
-	        std::cout << "  " << i << " " << cell->vertex_index(i) << " "
-	                << cell->vertex(i) << std::endl;
-	    }
+//	  {
+//	    std::vector<Point<spacedim>>         vertices;
+//	    std::vector<Tet::CellData<dim>> cells;
+//
+//	    if (spacedim == 2)
+//	    {
+//	    vertices.push_back(Point<spacedim>(1, 0));
+//	    vertices.push_back(Point<spacedim>(0, 1));
+//	    vertices.push_back(Point<spacedim>(0, 0));
+//	    vertices.push_back(Point<spacedim>(1, 1));
+//	    }
+//	    else
+//	    {
+//	    vertices.push_back(Point<spacedim>(1, 0, 0));
+//	    vertices.push_back(Point<spacedim>(0, 1, 0));
+//	    vertices.push_back(Point<spacedim>(0, 0, 0));
+//	    vertices.push_back(Point<spacedim>(1, 1, 0));
+//	    }
+//
+//	    Tet::CellData<dim> cell_1;
+//	    cell_1.type     = Tet::CellTypeEnum::tet;
+//	    cell_1.vertices = {0, 1, 2};
+//	    cells.push_back(cell_1);
+//
+//	    Tet::CellData<dim> cell_2;
+//	    cell_2.type     = Tet::CellTypeEnum::tet;
+//	    cell_2.vertices = {0, 1, 3};
+//	    cells.push_back(cell_2);
+//
+//	    tria.create_triangulation_tet(vertices, cells);
+//	  }
+//
+//	  // print cell-by-cell the vertices
+//	  for (const auto &cell : tria.cell_iterators())
+//	    {
+//	      for (unsigned int i = 0; i < cell->n_vertices(); i++)
+//	        std::cout << "  " << i << " " << cell->vertex_index(i) << " "
+//	                << cell->vertex(i) << std::endl;
+//	    }
 
 	  // TODO: write file for Paraview via GridOut
 
 
-//  GridIn<dim> grid_in;
-//  grid_in.attach_triangulation(triangulation);
-//  std::ifstream input_file("circle-grid.inp");
-//
-//  Assert(dim == 2, ExcInternalError());
-//
-//  grid_in.read_ucd(input_file);
-//
-//  const SphericalManifold<dim> boundary;
-//  triangulation.set_all_manifold_ids_on_boundary(0);
-//  triangulation.set_manifold(0, boundary);
-//
-//  for (unsigned int cycle = 0; cycle < 6; ++cycle)
-//    {
-//      std::cout << "Cycle " << cycle << ':' << std::endl;
-//
-//      if (cycle != 0)
-//        triangulation.refine_global(1);
-//
+  GridIn<dim, spacedim> grid_in;
+  grid_in.attach_triangulation(tria);
+
+  std::ifstream input_file("quad.inp");
+
+  Assert(dim == 2, ExcInternalError());
+
+  grid_in.read_ucd(input_file);
+
+  	  // print cell-by-cell the vertices
+  	  for (const auto &cell : tria.cell_iterators())
+  	    {
+  	      for (unsigned int i = 0; i < cell->n_vertices(); i++)
+  	        std::cout << "  " << i << " " << cell->vertex_index(i) << " "
+  	                << cell->vertex(i) << std::endl;
+  	    }
+
+  GridOut grid_out;
+  std::ofstream out("grid.vtk");
+  grid_out.write_vtk(tria, out);
+
+////
+////  const SphericalManifold<dim> boundary;
+////  triangulation.set_all_manifold_ids_on_boundary(0);
+////  triangulation.set_manifold(0, boundary);
+////
+////  for (unsigned int cycle = 0; cycle < 6; ++cycle)
+////    {
+////      std::cout << "Cycle " << cycle << ':' << std::endl;
+////
+////      if (cycle != 0)
+////        triangulation.refine_global(1);
+////
 //      // Now that we have a mesh for sure, we write some output and do all the
 //      // things that we have already seen in the previous examples.
 //      std::cout << "   Number of active cells: "  //
@@ -166,7 +189,7 @@ void Step5<dim>::run()
 //                << std::endl;
 //
 //
-//    }
+////    }
 }
 
 
@@ -176,7 +199,7 @@ void Step5<dim>::run()
 // won't comment on it further:
 int main()
 {
-  Step5<2> laplace_problem_2d;
-  laplace_problem_2d.run();
+  Tet_test<2> tet_test_2d;
+  tet_test_2d.run();
   return 0;
 }
